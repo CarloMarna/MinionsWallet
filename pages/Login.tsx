69594c804834fd4db61a3f5f02bd1609377e2628
Sergio Lembo <sergiolembo02@gmail.com>
2024-06-02 22:42:57 +0200

    prov

--- pages/Login.tsx
+++ pages/Login.tsx
@@ -18,7 +18,7 @@ const Login = ({ navigation, database, onLogin }) => {
     if (checkCredenziali.length > 0) {
       const id_conto = await database.getFirstAsync(`SELECT id FROM conto WHERE username = '${lowercaseUsername}'`);
       
-      onLogin(id_conto.id, username);
+      onLogin(id_conto.id, lowercaseUsername);
       navigation.navigate('HomePage');
     } else {
       Alert.alert('Errore', 'Username o password non validi');
