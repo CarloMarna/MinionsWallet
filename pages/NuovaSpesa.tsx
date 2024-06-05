90bab8832b50dcee75421a3aba61e9c6f99b0a53
andrea <andreavitolo87@gmail.com>
2024-06-05 18:30:25 +0200

    Fixed Color

--- pages/NuovaSpesa.tsx
+++ pages/NuovaSpesa.tsx
@@ -811,7 +811,7 @@ const styles = StyleSheet.create({
         flex: 1,
         justifyContent: 'center',
         alignItems: 'center',
-        backgroundColor: '#F5E642',
+        backgroundColor: '#FFF9C4',
     },
     textContainerCaricamento: {
         fontSize: 20,
